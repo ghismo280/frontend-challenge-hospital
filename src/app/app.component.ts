31b468ea67e8a2ee7c3843d1d0290f5efc519672
Angelo C <angelo.callari@soprasteria.com>
2019-06-26 21:51:03 +0200

    first output json in browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend-challenge-hospital';
+  constructor (private httpService: HttpClient) { }
+  resultArr: string [];
+
+  ngOnInit () {
+    this.httpService.get('../assets/mock-api-data/patients.json').subscribe(
+      data => {
+        this.resultArr = data as string [];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.resultArr[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+  }
 }
